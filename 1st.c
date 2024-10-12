520a7e5d7fad6828dd6cd078406b99c4a0d63236
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2024-10-12 23:24:59 +0530

    Create 1st.c
    Just a first program on C pretty basic

---
+++ 1st.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int A,B;
+    printf("Enter A");
+    scanf("%d",&A);
+    
+    printf("Enter B");
+    scanf("%d",&B);
+    
+    int sum = A + B;
+    printf("%d",sum);
+    return 0;
+}
