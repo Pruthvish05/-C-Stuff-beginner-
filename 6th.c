30a8ff885c02f3b996bdcc0a86a6bea1d8bbce85
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2024-10-14 22:46:59 +0530

    Create 6th.c
    Counting numbers

---
+++ 6th.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n;
+    printf("gimme a number :");
+    scanf("%d" , &n);
+    int i;
+    while(i<=n){
+        printf("%d" , i);
+        i++;
+        
+    }
+    return 0;
+}
